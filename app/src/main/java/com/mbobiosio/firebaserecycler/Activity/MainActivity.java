f35336fd8487626ab5c6c44f3f1a363ad72d6b55
hasancse91 <hasan_cse91@yahoo.com>
2017-03-22 14:07:28 +0600

    Expandable Layout added

--- app/src/main/java/com/mbobiosio/firebaserecycler/Activity/MainActivity.java
+++ app/src/main/java/com/mbobiosio/firebaserecycler/Activity/MainActivity.java
@@ -1,40 +1,51 @@
-package com.mbobiosio.firebaserecycler;
+package com.mbobiosio.firebaserecycler.Activity;
 
 import android.app.Application;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mbobiosio.firebaserecycler.R;
 import com.mbobiosio.firebaserecycler.model.ModelClass;
 import com.squareup.picasso.Picasso;
 
+import net.cachapa.expandablelayout.ExpandableLayout;
+
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView dataList;
+    private static RecyclerView recyclerView;
     FirebaseDatabase database;
     DatabaseReference dbRef;
+    private static final int UNSELECTED = -1;
+    private static int selectedItem = UNSELECTED;
+    private static Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
 
         /*Recycler View */
 
-        dataList = (RecyclerView) findViewById(R.id.list);
-        dataList.setHasFixedSize(true);
-        dataList.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         database = FirebaseDatabase.getInstance();
         dbRef = database.getReference("Data");
@@ -50,34 +61,76 @@ public class MainActivity extends AppCompatActivity {
                 DataListHolder.class,
                 dbRef) {
             @Override
-            protected void populateViewHolder(DataListHolder viewHolder, ModelClass model, int position) {
+            protected void populateViewHolder(final DataListHolder viewHolder, ModelClass model, final int position) {
                 viewHolder.setTitle(model.getTitle());
                 viewHolder.setImage(MainActivity.this, model.getImage());
                 viewHolder.setContent(model.getContent());
+                viewHolder.itemPosition = position;
+
+                viewHolder.cardView.setSelected(false);
+                viewHolder.expandableLayout.collapse(false);
+
+
+                viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DataListHolder dataListHolder = (DataListHolder) recyclerView.findViewHolderForAdapterPosition(selectedItem);
+
+                        if (dataListHolder != null) {
+                            dataListHolder.cardView.setSelected(false);
+                            dataListHolder.expandableLayout.collapse();
+                        }
+
+                        if (viewHolder.itemPosition == selectedItem) {
+                            selectedItem = UNSELECTED;
+                        } else {
+                            viewHolder.cardView.setSelected(true);
+                            viewHolder.expandableLayout.expand();
+                            selectedItem = position;
+                        }
+                    }
+                });
+
             }
+
         };
-        dataList.setAdapter(firebaseRecyclerAdapter);
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
     }
+
     public static class DataListHolder extends RecyclerView.ViewHolder {
+
+        private ExpandableLayout expandableLayout;
+        private TextView data_content;
+//        private TextView contentTextView;
+        private CardView cardView;
+        private int itemPosition;
         View mView;
 
         public DataListHolder(View itemView) {
             super(itemView);
             mView = itemView;
+            cardView = (CardView) mView.findViewById(R.id.recyclerHeader);
+            expandableLayout = (ExpandableLayout) itemView.findViewById(R.id.expandable_layout);
+            expandableLayout.setInterpolator(new OvershootInterpolator());
+            data_content = (TextView) mView.findViewById(R.id.content);
+//            contentTextView = (TextView) itemView.findViewById(R.id.content);
+
         }
         public void setTitle(String title) {
             TextView data_title = (TextView) mView.findViewById(R.id.titleText);
             data_title.setText(title);
         }
         public void setContent(String content) {
-            TextView data_content = (TextView) mView.findViewById(R.id.content);
             data_content.setText(content);
         }
         public void setImage(Context context, String image) {
             ImageView data_image = (ImageView) mView.findViewById(R.id.imageViewy);
             Picasso.with(context).load(image).into(data_image);
         }
+
     }
+
+
     public class Firebase extends Application {
         @Override
         public void onCreate() {
---
+++ app/src/main/java/com/mbobiosio/firebaserecycler/Adapter/MyAdapterClass.java
@@ -0,0 +1,54 @@
+package com.mbobiosio.firebaserecycler.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mbobiosio.firebaserecycler.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by hasan on 3/22/17.
+ */
+
+public class MyAdapterClass extends RecyclerView.Adapter<MyAdapterClass.DataListHolder>{
+
+    @Override
+    public DataListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(DataListHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class DataListHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public DataListHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setTitle(String title) {
+            TextView data_title = (TextView) mView.findViewById(R.id.titleText);
+            data_title.setText(title);
+        }
+        public void setContent(String content) {
+            TextView data_content = (TextView) mView.findViewById(R.id.content);
+            data_content.setText(content);
+        }
+        public void setImage(Context context, String image) {
+            ImageView data_image = (ImageView) mView.findViewById(R.id.imageViewy);
+            Picasso.with(context).load(image).into(data_image);
+        }
+    }
+}
