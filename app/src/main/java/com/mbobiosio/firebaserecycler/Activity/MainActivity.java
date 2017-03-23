d295cef8eaa4a6cc18a24c015e4ac4443710c8cc
cazewonder <cazewonder@gmail.com>
2017-03-23 22:03:52 +0100

    Added Toolbar and Coordinator Layout

--- app/src/main/java/com/mbobiosio/firebaserecycler/Activity/MainActivity.java
+++ app/src/main/java/com/mbobiosio/firebaserecycler/Activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         context = this;
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
